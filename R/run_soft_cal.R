e17419b7fe76bc2f6280cee95b7a4ecc08b97401
Moritz Shore <moritzshore@gmail.com>
2023-02-02 14:07:55 -0500

    read_wb_aa() converts to numeric()
    Converts the correct columns to numeric (excluding name and description)

--- R/run_soft_cal.R
+++ R/run_soft_cal.R
@@ -7,29 +7,49 @@ library(tidyr) # unite
 
 
 # this will need to be determined / passed
-PATH = "C:/Users/NIBIO/Documents/GitLab/optain-swat/SWAT_softcal/swatplus_rev60_demo/"
+path = "C:/Users/NIBIO/Documents/GitLab/optain-swat/SWAT_softcal/swatplus_rev60_demo/"
 
-# read the wb_aa file from its PATH
-basin_wb_aa = fread(paste0(PATH, "basin_wb_aa.txt"), fill = TRUE)
+
 
 # The following is only required due to the poor formatting of the SWAT output
+read_wb_aa <- function(path){
+  # read the wb_aa file from its PATH
+  basin_wb_aa = fread(paste0(path, "basin_wb_aa.txt"), fill = TRUE, )
+  
+  
+  # change the column names to those of the second row in the text file. 
+  colnames(basin_wb_aa) <- basin_wb_aa %>% slice(2) %>% unlist(., use.names = F)
+  
+  # add placeholder names for the last three columns which did not get a name
+  colnames(basin_wb_aa)[c(47:49)] <- c("x", "y", "z")
+  
+  # remove rows 1 to 3, as they don't contain any real data
+  basin_wb_aa <- basin_wb_aa %>% filter(!row_number() %in% c(1:3))
+  
+  # Unite the last three rows into one string (as they were intended to be)
+  # the new column will be named description and describes what the softcal
+  # algorithm did in that step. (So its probably important).
+  # the united columns are separated by a space (trailing space exists now)
+  basin_wb_aa = tidyr::unite(data = basin_wb_aa,
+                             col = description,
+                             c(47:49),
+                             sep = " ") 
+  
+  
+  # figure out which columns should be a double and not a string
+  # (everything except for name and decription columns)
+  dbl_cols = basin_wb_aa %>% colnames()
+  dbl_cols= dbl_cols[! dbl_cols %in% c("name","description")]
+  # and convert them 
+  basin_wb_aa = basin_wb_aa %>% mutate_at(dbl_cols, as.numeric) %>% tibble()
+  
+  return(basin_wb_aa)
+}
 
-# change the column names to those of the second row in the text file. 
-colnames(basin_wb_aa) <- basin_wb_aa %>% slice(2) %>% unlist(., use.names = F)
 
-# add placeholder names for the last three columns which did not get a name
-colnames(basin_wb_aa)[c(47:49)] <- c("x", "y", "z")
 
-# remove rows 1 to 3, as they don't contain any real data
-basin_wb_aa <- basin_wb_aa %>% filter(!row_number() %in% c(1:3))
+df = read_wb_aa(path)
+library(ggplot2)
+ggplot(df)+geom_col(mapping = aes(x = description, y = wateryld ))
 
-# Unite the last three rows into one string (as they were intended to be)
-# the new column will be named description and describes what the softcal
-# algorithm did in that step. (So its probably important).
-# the united columns are separated by a space (trailing space exists now)
-basin_wb_aa = tidyr::unite(data = basin_wb_aa,
-                           col = description,
-                           c(47:49),
-                           sep = " ") 
-
-# Now back to work:
+df %>% tibble()
